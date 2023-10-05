be1ec97e5c63e17d0e5cd0e49d7ec79b65754bba
sambutracy <tracysambut@gmail.com>
2023-10-05 12:58:30 -0700

    Hello print f:

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ * Return: 0 if the process is successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
