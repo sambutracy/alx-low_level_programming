7b9f0f84c357d0f6cfa3b778374f8621a9daa7a3
sambutracy <tracysambut@gmail.com>
2023-10-06 14:34:12 -0700

    more printed alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Defines the entry point
+ * Return: success
+ */
+
+int main(void)
+
+{
+	char low;
+	for (low = 'a'; low<= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
