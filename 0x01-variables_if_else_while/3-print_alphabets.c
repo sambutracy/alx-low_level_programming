b3571ef50e2f2a07e1d81a3b33f71cc74e785ea3
sambutracy <tracysambut@gmail.com>
2023-10-06 14:51:50 -0700

    more printed alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-    char low;
+	char low;
 
-    for (low = 'a'; low <= 'z'; low++)
-        putchar(low);
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
 
-    for (low = 'A'; low <= 'Z'; low++)
-        putchar(low);
-    putchar('\n');
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
