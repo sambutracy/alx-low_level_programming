2fd2456523768e0b2a72659794b3d8b318342e00
sambutracy <tracysambut@gmail.com>
2023-10-11 11:54:52 -0700

    alphabets

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Return: Always 0.
+ * main -check the code
+ * Return: Always return 0 if successful
  */
-
 int main(void)
 {
 	print_alphabet();
