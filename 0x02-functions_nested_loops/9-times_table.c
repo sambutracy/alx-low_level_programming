6f6abe1a52a471967f5260beeba8634d9f294dcf
sambutracy <tracysambut@gmail.com>
2023-10-12 11:43:26 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,28 @@ void times_table(void)
 {
 	int row, column, product;
 
-	for (row = 0; row <= 10; row++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (column = 0; column <= 10; column++)
+		for (column = 0; column <= 9; column++)
 		{
 			product = row * column;
+
 			if (column == 0)
 			{
 				_putchar(product + '0');
 			}
-			if (product < 10 && column != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-			else if (product >= 10)
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
 			}
-			{
-				_putchar('\n');
-			}
+				if (product < 10)
+				{
+					_putchar(' ');
+				}
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
