f976900f5ef1e436a96fc8708aea680aa71b70be
sambutracy <tracysambut@gmail.com>
2023-10-14 15:30:55 -0700

    betty errors

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a >= b && a >= c)
 	{
 		largest = a;
@@ -20,7 +20,8 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else{
+	else
+	{
 		largest = c;
 	}
 	return (largest);
