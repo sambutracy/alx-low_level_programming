3b7e539de8b52697124662a4e7f9ce3684b83815
sambutracy <tracysambut@gmail.com>
2023-10-19 16:11:25 -0700

    encode string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int t, s;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+	
+	for (t = 0; n[t] != '\0';
+			t++)
+	{
+		for (s = 0; s < 10; s++)
+		{
+			if (n[t] == s1[s])
+			{
+				n[t] = s2[s];
+			}
+		}
+	}
+	return (n);
+}
