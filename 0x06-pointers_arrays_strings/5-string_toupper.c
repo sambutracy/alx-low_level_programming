9b016196aacdea502cce3ae53993b33d4c7044f0
sambutracy <tracysambut@gmail.com>
2023-10-19 15:55:38 -0700

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - transforms all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int t;
+	
+	t = 0;
+	while (n[t] != '\0')
+	{
+		if (n[t] >= 'a' && n[t] <= 'z')
+			n[t] = n[t] - 32;
+		t++;
+	}
+	return (n);
+}
