57f9b3aa464fca237a82950c1e4d79aa0baad7ae
sambutracy <tracysambut@gmail.com>
2023-10-25 15:34:01 -0700

    put recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - the main function
+ * @s: the function parameter
+ *
+ * Return: 0 if successful
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1); 
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
-#ifdef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -9,4 +10,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _putchar(char c);
 #endif
