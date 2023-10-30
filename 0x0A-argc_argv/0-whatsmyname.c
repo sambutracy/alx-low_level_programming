dd15fb005c927700ba93576f7b50223b06690662
sambutracy <tracysambut@gmail.com>
2023-10-30 11:20:39 -0700

    error coorection

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - print name of program
  * @argc: argument count
@@ -8,7 +9,7 @@ int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("%s\n", *argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
