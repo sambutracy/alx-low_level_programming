6f00d6bed52a14bf9ea16b77e8b425e8e914b1a3
sambutracy <tracysambut@gmail.com>
2023-10-30 11:36:16 -0700

    please work

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,14 @@
 /**
  * main - print name of program
  * @argc: argument count
- * @argv: argument vecto
+ * @argv: argument vector
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	}
 	return (0);
 }
