fc5f772dd049d25e41c150e39892cb027055c07d
sambutracy <tracysambut@gmail.com>
2023-11-22 13:57:41 -0800

    betty update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
