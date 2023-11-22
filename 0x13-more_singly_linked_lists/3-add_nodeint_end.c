c7e145fb1860ee42a56fd539079c4f55a89e2acd
sambutracy <tracysambut@gmail.com>
2023-11-22 13:59:43 -0800

    betty update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,13 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * add_nodeint_end - Adds a new node at the end of a listint_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @n: The data to be added to the new node.
+ * Return: The address of the new element, or NULL if it failed.
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *temp;
